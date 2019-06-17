b8ec6dea14973177332cc4e3d6592561c086d8e6
Brennan Richards <brennanr0210@gmail.com>
2019-06-17 00:58:19 -0400

    Completed article createfunctionality via UI.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,21 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was successfully created."
+    else
+      render 'new'
+    end
 
     redirect_to article_path(@article)
   end
+
+  def show
+
+    @article = Article.find(params[:id])
+
+  end
+
 
   private
 
